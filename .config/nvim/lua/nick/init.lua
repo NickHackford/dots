8f3197f0c77b94fc1d1dbf35f5b900f79d58bfcd
Nick Hackford <Nick.Hackford@gmail.com>
2023-04-28 18:30:20 -0400

    add packages and format config

--- .config/nvim/lua/nick/init.lua
+++ .config/nvim/lua/nick/init.lua
@@ -1,5 +1,22 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local yank_group = augroup("HighlightYank", {})
+
+-- Highlight text on yank
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
 require("nick.set")
 require("nick.remap")
