ca275b570452eaff4d64532448ccbc3545f8d053
Nick Hackford <Nick.Hackford@gmail.com>
2023-05-02 17:22:20 -0400

    more fiddling with nvim and tmux

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,6 @@
 return {
 	"ThePrimeagen/harpoon",
+	event = "VimEnter",
 	config = function()
 		require("harpoon").setup({
 			menu = {
@@ -11,18 +12,18 @@ return {
 		local ui = require("harpoon.ui")
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<leader>vh", ui.toggle_quick_menu, {}, "view harpoon")
+		vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, {}, "view harpoon")
 
-		vim.keymap.set("n", "<C-j>", function()
+		vim.keymap.set("n", "<leader>j", function()
 			ui.nav_file(1)
 		end)
-		vim.keymap.set("n", "<C-k>", function()
+		vim.keymap.set("n", "<leader>k", function()
 			ui.nav_file(2)
 		end)
-		vim.keymap.set("n", "<C-l>", function()
+		vim.keymap.set("n", "<leader>l", function()
 			ui.nav_file(3)
 		end)
-		vim.keymap.set("n", "<C-;>", function()
+		vim.keymap.set("n", "<leader>;", function()
 			ui.nav_file(4)
 		end)
 	end,
