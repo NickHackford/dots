46f3dd0d95dd4e070a9fc2c901af64d6b8478584
Nick Hackford <nhackford@wayfair.com>
2023-05-02 00:15:09 -0400

    update to lazy

---
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,9 @@
+return {
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		-- in diff, grab left or right
+		vim.keymap.set("n", "gj", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "gf", "<cmd>diffget //3<CR>")
+	end,
+}
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	-- Tmux
+	"christoomey/vim-tmux-navigator",
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
+	-- gc to comment
+	"tpope/vim-commentary",
+	-- sa to surround
+	"machakann/vim-sandwich",
+}
