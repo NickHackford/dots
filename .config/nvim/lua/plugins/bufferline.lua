fa1bbdb71d0704e96bfcba2298c2ad4de0c18b33
Nick Hackford <Nick.Hackford@gmail.com>
2023-05-04 18:43:43 -0400

    add bufferline maps

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,19 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
-		require("bufferline").setup()
+		-- nnoremap mymap :lua require"bufferline".go_to(num)<CR>
+		local bufferline = require("bufferline")
+		vim.keymap.set("n", "<leader>bd", ":bd")
+
+		vim.keymap.set("n", "<leader>b1", ":lua require('bufferline').go_to(1)<CR>")
+		vim.keymap.set("n", "<leader>b2", ":lua require('bufferline').go_to(2)<CR>")
+		vim.keymap.set("n", "<leader>b3", ":lua require('bufferline').go_to(3)<CR>")
+		vim.keymap.set("n", "<leader>b4", ":lua require('bufferline').go_to(4)<CR>")
+		vim.keymap.set("n", "<leader>b5", ":lua require('bufferline').go_to(5)<CR>")
+
+		vim.keymap.set("n", "<leader>bh", ":BufferLineCyclePrev<CR>")
+		vim.keymap.set("n", "<leader>bl", ":BufferLineCycleNext<CR>")
+
+		bufferline.setup()
 	end,
 }
