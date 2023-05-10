e52e496a22482dbb8733b5cd46d46c12e58b6933
Nick Hackford <nhackford@wayfair.com>
2023-05-10 11:55:41 -0400

    add rndr script

--- bin/.local/scripts/tmux-cht.sh
+++ bin/.local/scripts/tmux-cht.sh
@@ -10,10 +10,8 @@ read -p "Enter Query: " query
 if grep -qs "$selected" ~/.config/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     echo $query
-    echo "goo"
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    echo "foo"
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
 
