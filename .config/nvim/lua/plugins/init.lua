147757e44e6ad0b041c0634887c12624533d022f
Nick Hackford <Nick.Hackford@gmail.com>
2023-07-13 13:49:41 -0400

    updates for nixos

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,11 +1,9 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	-- Detect tabstop and shiftwidth automatically
-	"tpope/vim-sleuth",
-	-- gc to comment
-	"tpope/vim-commentary",
-	-- sa to surround
-	"machakann/vim-sandwich",
-	"alvan/vim-closetag",
-	"mg979/vim-visual-multi",
+  "christoomey/vim-tmux-navigator",
+  -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-sleuth",
+  -- sa to surround
+  "machakann/vim-sandwich",
+  "alvan/vim-closetag",
+  "mg979/vim-visual-multi",
 }
