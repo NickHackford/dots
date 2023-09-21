29bbe5f21316ff32df00f3e76f77391ac1311263
Nick Hackford <nhackford@wayfair.com>
2023-09-21 13:58:19 -0400

    add nix for my mac

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  home.username = "nh470c";
+  home.homeDirectory = "/Users/nh470c";
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    btop
+    fzf
+    highlight
+    lf
+    neofetch
+    neovim
+    nixfmt
+    ripgrep
+    tmux
+  ];
+
+  programs.git = { enable = true; };
+}
