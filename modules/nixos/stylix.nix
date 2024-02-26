d7554d5b40360332cd69633012f21c93a5c3b780
Nick Hackford <Nick.Hackford@gmail.com>
2024-02-25 21:36:17 -0500

    tweak(stylix): increase font size

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -5,10 +5,8 @@
 
     # base16Scheme =
     #   "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
 
     fonts = {
-      sizes.applications = 10;
       monospace = {
         package = pkgs.nerdfonts.override { fonts = [ "SourceCodePro" ]; };
         name = "SauceCodePro Nerd Font Mono";
