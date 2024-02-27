b04948cee89ca2096315dc8fa17808743f56567b
Nick Hackford <Nick.Hackford@gmail.com>
2024-02-27 01:35:19 -0500

    feat(nvim):improve tmux integration and clean config

---
+++ files/config/nvim/lua/plugins/better-vim-tmux-resizer.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-w>h", "<cmd>TmuxResizeLeft<CR>")
+vim.keymap.set("n", "<C-w>j", "<cmd>TmuxResizeDown<CR>")
+vim.keymap.set("n", "<C-w>k", "<cmd>TmuxResizeUp<CR>")
+vim.keymap.set("n", "<C-w>l", "<cmd>TmuxResizeRight<CR>")
