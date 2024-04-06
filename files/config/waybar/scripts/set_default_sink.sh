901f3636fc111c764a97d1fd4c364dd77b167947
Nick Hackford <nhackford@wayfair.com>
2024-04-06 16:50:51 -0400

    feat(flake): bump flake and update pipewire config

--- files/config/waybar/scripts/set_default_sink.sh
+++ files/config/waybar/scripts/set_default_sink.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 command_output=$(wpctl status)
-search_string="Audio/Sink    alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.playback.0.0"
+search_string="Audio/Sink    alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo"
 
 if grep -q "$search_string" <<< "$command_output"; then
     wpctl set-default $(wpctl status | grep -m 1 -A 6 "Sinks" | grep "Soundbar" | sed 's/[^0-9]*\([0-9]*\).*/\1/')
