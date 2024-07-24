19a7d33ad409c7d60e86d5864df80e41d7b707a4
Nick Hackford <Nick.Hackford@gmail.com>
2024-07-24 18:27:25 +0000

    feat(nvim): config trouble

--- files/config/nvim/lua/plugins/trouble.lua
+++ files/config/nvim/lua/plugins/trouble.lua
@@ -4,16 +4,23 @@ return {
 	config = function()
 		vim.keymap.set(
 			"n",
-			"<leader>vq",
-			"<cmd>TroubleToggle quickfix<cr>",
+			"<leader>tq",
+			"<cmd>Trouble diagnostics toggle<cr>",
 			{ desc = "View Quickfix", silent = true, noremap = true }
 		)
+		vim.keymap.set(
+			"n",
+			"<leader>tn",
+			"<cmd>Trouble diagnostics next<cr><cmd>Trouble diagnostics focus<cr>",
+			{ desc = "Trouble next diagnostic", silent = true, noremap = true }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>tp",
+			"<cmd>Trouble diagnostics prev<cr><cmd>Trouble diagnostics focus<cr>",
+			{ desc = "Trouble prev diagnostic", silent = true, noremap = true }
+		)
 
-		require("trouble").setup({
-			icons = false,
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		})
+		require("trouble").setup()
 	end,
 }
