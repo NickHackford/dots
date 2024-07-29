1d31463a440b82362b865ccd6de7cc68e3de2122
Nick Hackford <nicholas.hackford@claconnect.com>
2024-07-29 18:39:09 +0000

    feat(tmux): improve tmux popup

--- files/local/bin/tmux_popup.sh
+++ files/local/bin/tmux_popup.sh
@@ -2,8 +2,8 @@
 
 width=${2:-80%}
 height=${2:-80%}
-if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ]; then
+if [[ "$(tmux display-message -p -F "#{session_name}")" == *"_popup" ]]; then
     tmux detach-client
 else
-    tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -E "tmux attach -t popup || tmux new -s popup"
+    tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -E "tmux attach -t $(tmux display-message -p -F "#{session_name}")_popup || tmux new -s $(tmux display-message -p -F "#{session_name}")_popup"
 fi
