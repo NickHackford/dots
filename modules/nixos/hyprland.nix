1771a60f2c3817a06f7a61d2470fbfd5e13c00e3
Nick Hackford <Nick.Hackford@gmail.com>
2024-09-06 09:10:42 -0400

    feat(lock): remove old swaylock config

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -46,13 +46,6 @@
     };
   };
 
-  # TODO: What is this?
-  security.pam.services.swaylock = {
-    text = ''
-      auth include login
-    '';
-  };
-
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
