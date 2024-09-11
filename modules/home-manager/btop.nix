96c9762cc46d72be247e3b8fd7e7c1a288fecd6a
Nick Hackford <nicholas.hackford@claconnect.com>
2024-09-11 13:37:00 -0400

    feat(btop): change celsius to fahrenheit

--- modules/home-manager/btop.nix
+++ modules/home-manager/btop.nix
@@ -11,6 +11,7 @@
       rounded_corners = true;
       vim_keys = true;
       background_update = false;
+      temp_scale = "fahrenheit";
       presets = "cpu:0:default,mem:0:default";
       # TODO: add preset for gpu
     };
