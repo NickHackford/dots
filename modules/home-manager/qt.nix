90b909f766d408885c00c5decaa6a27810a003bb
Nick Hackford <Nick.Hackford@gmail.com>
2024-09-13 01:13:27 -0400

    rice(gtk)

---
+++ modules/home-manager/qt.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}: let
+in {
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
+  };
+}
