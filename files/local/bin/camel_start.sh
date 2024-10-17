3879f039d2900c8dcdd975bf0a1bee42ee392540
Nick Hackford <nicholas.hackford@claconnect.com>
2024-10-17 16:36:08 -0400

    chore: remove cla scripts

--- files/local/bin/camel_start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-az account set --subscription "Auto Extract Integration Test"
-az vm start --name ae-camel -g productivity
--- files/local/bin/vm_start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-az account set --subscription "EA - Sandbox"
-az vm start -g ea-sbx-dc01-containerdev-rg -n ea-sbx-dc01-containerdev-vm283
