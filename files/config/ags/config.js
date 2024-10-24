780c69938c697131db0fa6ca2fbe54d55209079c
Nick Hackford <nicholas.hackford@claconnect.com>
2024-10-24 16:26:02 -0400

    feat(wofi): replace wofi with ags

--- files/config/ags/config.js
+++ files/config/ags/config.js
@@ -2,7 +2,7 @@ import { Bar } from "./bar.js";
 import { SystemPopup } from "./bar.js";
 import { NotificationPopups } from "./notifications.js";
 import { SoundNotificationWatcher } from "./sound-notifications-watcher.js";
-// import { AppLauncherWindow } from "./app-launcher.js";
+import { AppLauncherWindow } from "./app-launcher.js";
 
 App.config({
   style: App.configDir + "/style.css",
@@ -11,6 +11,6 @@ App.config({
     SystemPopup(1),
     NotificationPopups(1),
     SoundNotificationWatcher(),
-    // AppLauncherWindow,
+    AppLauncherWindow,
   ],
 });
