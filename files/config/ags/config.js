d624d825bfef76e62bb4f53e957bd4be5799653c
Nick Hackford <Nick.Hackford@gmail.com>
2024-10-24 16:05:03 -0400

    feat(wlogout): replace wlogout with ags
    temp

--- files/config/ags/config.js
+++ files/config/ags/config.js
@@ -1,8 +1,16 @@
 import { Bar } from "./bar.js";
+import { SystemPopup } from "./bar.js";
 import { NotificationPopups } from "./notifications.js";
 import { SoundNotificationWatcher } from "./sound-notifications-watcher.js";
+// import { AppLauncherWindow } from "./app-launcher.js";
 
 App.config({
   style: App.configDir + "/style.css",
-  windows: [Bar(1), NotificationPopups(1), SoundNotificationWatcher()],
+  windows: [
+    Bar(1),
+    SystemPopup(1),
+    NotificationPopups(1),
+    SoundNotificationWatcher(),
+    // AppLauncherWindow,
+  ],
 });
