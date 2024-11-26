04af583b090553a8560332f0ec19e472783dbd00
Nick Hackford <nick.hackford@gmail.com>
2024-11-25 21:50:33 -0500

    feat(ags): support TV

--- files/config/ags/config.js
+++ files/config/ags/config.js
@@ -1,16 +1,29 @@
+const Gdk = imports.gi.Gdk;
+const display = Gdk.Display.get_default();
+
 import { Bar } from "./bar.js";
 import { SystemPopup } from "./bar.js";
 import { NotificationPopups } from "./notifications.js";
 import { SoundNotificationWatcher } from "./sound-notifications-watcher.js";
 import { AppLauncherWindow } from "./app-launcher.js";
 
+function getAWMonitor() {
+  const numMonitors = display.get_n_monitors();
+  for (let id = 0; id < numMonitors; ++id) {
+    if (display.get_monitor(id).model.includes("AW")) {
+      return id;
+    }
+  }
+}
+const AWMonitorId = getAWMonitor();
+
 App.config({
   style: App.configDir + "/style.css",
   windows: [
-    Bar(1),
-    SystemPopup(1),
-    NotificationPopups(1),
-    SoundNotificationWatcher(1),
+    Bar(AWMonitorId),
+    SystemPopup(AWMonitorId),
+    NotificationPopups(AWMonitorId),
+    SoundNotificationWatcher(AWMonitorId),
     AppLauncherWindow,
   ],
 });
