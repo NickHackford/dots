b889bf33ffd182a595c1b1439228706e829bac7b
Nick Hackford <nhackford@hubspot.com>
2024-12-17 12:22:45 -0500

    feat(nvim): add snacks, large refactor

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -3,15 +3,6 @@
   pkgs,
   ...
 }: let
-  harpoon2 = pkgs.vimUtils.buildVimPlugin {
-    name = "harpoon2";
-    src = pkgs.fetchFromGitHub {
-      owner = "ThePrimeagen";
-      repo = "harpoon";
-      rev = "7d1aef462a880fcb68419cb63abc50dbdc22d922";
-      hash = "sha256-AXWN7HqlnSsmtCK8jK5vqyzHwKJY3eJL6fnjeJhoNMU=";
-    };
-  };
   better-vim-tmux-resizer = pkgs.vimUtils.buildVimPlugin {
     name = "better-vim-tmux-resizer";
     src = pkgs.fetchFromGitHub {
@@ -19,24 +10,6 @@
       repo = "better-vim-tmux-resizer";
       rev = "a791fe5b4433ac43a4dad921e94b7b5f88751048";
       hash = "sha256-1uHcQQUnViktDBZt+aytlBF1ZG+/Ifv5VVoKSyM9ML0=";
-    };
-  };
-  obsidian-nvim = pkgs.vimUtils.buildVimPlugin {
-    name = "obsidian-nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "epwalsh";
-      repo = "obsidian.nvim";
-      rev = "ebf4cd26f8c69e6ed025d06dfb4144212817878b";
-      hash = "sha256-8JpbqzbAs6fvM3iyyWszBl1LIp1tqNJ4Ngep0EunCRg=";
-    };
-  };
-  yazi-nvim = pkgs.vimUtils.buildVimPlugin {
-    name = "yazi-nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "mikavilpas";
-      repo = "yazi.nvim";
-      rev = "a4e9d2ce9e88dcc146ac621cbb2373e8a5e765c3";
-      hash = "sha256-dQ+KFbFtm3XflQl7optMfceR8F3dtF+11Ly6AGSUtkI=";
     };
   };
   asset-bender-nvim = pkgs.vimUtils.buildVimPlugin {
@@ -57,6 +30,15 @@
       hash = "sha256-oiGWlyt9+zN7ReQtdtVqFHVeaoypg6vFNOwSr9d3I2Y=";
     };
   };
+  my-CopilotChat-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "CopilotChat.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "CopilotC-Nvim";
+      repo = "CopilotChat.nvim";
+      rev = "2ebe591cff06018e265263e71e1dbc4c5aa8281e";
+      hash = "sha256-IPP5jXIX+05Tb0MEXUu6EjcL/RHgV1qkoXPEdaEfhNM=";
+    };
+  };
 
   hubspotPlugins =
     if config.isHubspot
@@ -72,86 +54,73 @@ in {
     extraLuaPackages = luaPkgs: with luaPkgs; [luasocket];
     plugins = with pkgs.vimPlugins;
       [
+        # Theming
         tokyonight-nvim
         nightfox-nvim
         kanagawa-nvim
-        nvim-notify
         nvim-web-devicons
 
-        # nvim-dap
-        # nvim-dap-ui
-
+        # LSP and Autocompletion
+        nvim-cmp
         nvim-lspconfig
         cmp-nvim-lsp
+        lspkind-nvim
         luasnip
         cmp_luasnip
         #codeium-nvim
         copilot-lua
-        CopilotChat-nvim
+        my-CopilotChat-nvim
         copilot-cmp
-        nvim-cmp
+        # nvim-dap
+        # nvim-dap-ui
 
-        lspkind-nvim
-
-        alpha-nvim
+        # UI Enhancements
         bufferline-nvim
-        formatter-nvim
         gitsigns-nvim
-
-        vim-hexokinase
-        harpoon2
-
-        lazygit-nvim
         lualine-nvim
-        markdown-preview-nvim
-        render-markdown-nvim
+        snacks-nvim
+        trouble-nvim
+        which-key-nvim
 
+        # Navigation
+        harpoon2
         nvim-tree-lua
-        nvim-treesitter.withAllGrammars
-
-        nvim-treesitter-context
-
-        obsidian-nvim
-        oil-nvim
-        playground
-        sniprun
         telescope-fzf-native-nvim
         telescope-nvim
-        trouble-nvim
+        yazi-nvim
 
+        # Markdown and Writing
+        markdown-preview-nvim
+        render-markdown-nvim
+        obsidian-nvim
+        vim-markdown-toc
+
+        # Treesitter and Syntax
+        nvim-treesitter.withAllGrammars
+        nvim-treesitter-context
+        playground
         vim-closetag
         vim-commentary
         nvim-ts-context-commentstring
-        vim-fugitive
-        vim-markdown-toc
+
+        # Utilities
+        formatter-nvim
+        vim-hexokinase
+        oil-nvim
+        sniprun
         vim-sandwich
         vim-table-mode
         vim-tmux-navigator
         better-vim-tmux-resizer
         vim-visual-multi
-        which-key-nvim
-        yazi-nvim
       ]
       ++ hubspotPlugins;
-    extraLuaConfig = ''
-      require('core.nix-init')
-    '';
   };
-  home.file = {
-    "nvim-settings" = {
-      source = ../../files/config/nvim/lua/core;
-      target = ".config/nvim/lua/core";
-      recursive = true;
-    };
-    "nvim-plugins" = {
-      source = ../../files/config/nvim/lua/plugins;
 
-      target = ".config/nvim/lua/plugins";
-      recursive = true;
-    };
-    "snippets" = {
-      source = ../../files/config/nvim/snippets;
-      target = ".config/nvim/snippets";
+  home.file = {
+    "nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.homeDirPath}.config/dots/files/config/nvim";
+      target = ".config/nvim";
       recursive = true;
     };
   };
