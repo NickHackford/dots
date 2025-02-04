c925fb4e4bf1f8c184261927358ce10f68b5a784
Nick Hackford <nhackford@hubspot.com>
2025-02-04 10:38:51 -0500

    feat(ssh): modify ssh config for hubspot

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  config,
+  ...
+}: let
   linuxPackages =
     if pkgs.stdenv.isLinux
     then
@@ -16,6 +20,20 @@
     }
     else {
       nr = "darwin-rebuild switch --flake ~/.config/dots";
+    };
+  hubspotFiles =
+    if config.isHubspot
+    then {
+      ".ssh/custom_ssh_config" = {
+        source = ../../files/config/ssh/config;
+        target = ".ssh/custom_ssh_config";
+      };
+    }
+    else {
+      ".ssh/config" = {
+        source = ../../files/config/ssh/config;
+        target = ".ssh/config";
+      };
     };
 in {
   programs.zsh.enable = true;
@@ -66,71 +84,68 @@ in {
     ]
     ++ linuxPackages;
 
-  home.file = {
-    "bin" = {
-      source = ../../files/local/bin;
-      target = ".local/bin";
-      recursive = true;
-    };
-    "cht" = {
-      source = ../../files/local/share/cht;
-      target = ".local/share/cht";
-      recursive = true;
-    };
+  home.file =
+    {
+      "bin" = {
+        source = ../../files/local/bin;
+        target = ".local/bin";
+        recursive = true;
+      };
+      "cht" = {
+        source = ../../files/local/share/cht;
+        target = ".local/share/cht";
+        recursive = true;
+      };
 
-    "nvm.plugin.zsh" = {
-      source = ../../files/config/zsh/nvm.plugin.zsh;
-      target = ".config/zsh/nvm.plugin.zsh";
-    };
-    ".p10l.zsh" = {
-      source = ../../files/config/zsh/.p10k.zsh;
-      target = ".config/zsh/.p10k.zsh";
-    };
-    # ".zshrc" = {
-    #   source = ../../files/zshrc;
-    #   target = ".zshrc";
-    # };
-    "powerlevel10k" = {
-      source = builtins.fetchGit {
-        url = "https://github.com/romkatv/powerlevel10k.git";
-        ref = "master";
-        rev = "bd0fa8a08f62a6e49f8a2ef47f5103fa840d2198";
+      "nvm.plugin.zsh" = {
+        source = ../../files/config/zsh/nvm.plugin.zsh;
+        target = ".config/zsh/nvm.plugin.zsh";
       };
-      target = ".config/zsh/plugins/powerlevel10k";
-    };
-    "zsh-autosuggestions" = {
-      source = builtins.fetchGit {
-        url = "https://github.com/zsh-users/zsh-autosuggestions.git";
-        ref = "master";
-        rev = "c3d4e576c9c86eac62884bd47c01f6faed043fc5";
+      ".p10l.zsh" = {
+        source = ../../files/config/zsh/.p10k.zsh;
+        target = ".config/zsh/.p10k.zsh";
       };
-      target = ".config/zsh/plugins/zsh-autosuggestions";
-    };
-    "zsh-syntax-highlighting" = {
-      source = builtins.fetchGit {
-        url = "https://github.com/zsh-users/zsh-syntax-highlighting.git";
-        ref = "master";
-        rev = "e0165eaa730dd0fa321a6a6de74f092fe87630b0";
+      # ".zshrc" = {
+      #   source = ../../files/zshrc;
+      #   target = ".zshrc";
+      # };
+      "powerlevel10k" = {
+        source = builtins.fetchGit {
+          url = "https://github.com/romkatv/powerlevel10k.git";
+          ref = "master";
+          rev = "bd0fa8a08f62a6e49f8a2ef47f5103fa840d2198";
+        };
+        target = ".config/zsh/plugins/powerlevel10k";
       };
-      target = ".config/zsh/plugins/zsh-syntax-highlighting";
-    };
-    "zsh-vim" = {
-      source = builtins.fetchGit {
-        url = "https://github.com/zap-zsh/vim.git";
-        ref = "master";
-        rev = "46284178bcad362db40509e1db058fe78844d113";
+      "zsh-autosuggestions" = {
+        source = builtins.fetchGit {
+          url = "https://github.com/zsh-users/zsh-autosuggestions.git";
+          ref = "master";
+          rev = "c3d4e576c9c86eac62884bd47c01f6faed043fc5";
+        };
+        target = ".config/zsh/plugins/zsh-autosuggestions";
       };
-      target = ".config/zsh/plugins/vim";
-    };
+      "zsh-syntax-highlighting" = {
+        source = builtins.fetchGit {
+          url = "https://github.com/zsh-users/zsh-syntax-highlighting.git";
+          ref = "master";
+          rev = "e0165eaa730dd0fa321a6a6de74f092fe87630b0";
+        };
+        target = ".config/zsh/plugins/zsh-syntax-highlighting";
+      };
+      "zsh-vim" = {
+        source = builtins.fetchGit {
+          url = "https://github.com/zap-zsh/vim.git";
+          ref = "master";
+          rev = "46284178bcad362db40509e1db058fe78844d113";
+        };
+        target = ".config/zsh/plugins/vim";
+      };
 
-    ".ssh/config" = {
-      source = ../../files/config/ssh/config;
-      target = ".ssh/config";
-    };
-
-    "fastfetch" = {
-      source = ../../files/config/fastfetch/config.jsonc;
-      target = ".config/fastfetch/config.jsonc";
-    };
-  };
+      "fastfetch" = {
+        source = ../../files/config/fastfetch/config.jsonc;
+        target = ".config/fastfetch/config.jsonc";
+      };
+    }
+    // hubspotFiles;
 }
