9c8006fda96e37a120fc43cae65b09a785e936f3
Nick Hackford <Nick.Hackford@gmail.com>
2025-02-18 12:34:17 -0500

    bug(sketchybar): fix audio device toggle

--- files/config/sketchybar/plugins/toggle_audio_output.sh
+++ files/config/sketchybar/plugins/toggle_audio_output.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-current_device=$(SwitchAudioSource -c)
+current_device=$(/opt/homebrew/bin/SwitchAudioSource -c)
 
-device_1="Razer Barracuda Pro 2.4"
-device_2="MacBook Pro Speakers"
+device_1="MacBook Pro Speakers"
+device_2="Razer Barracuda Pro 2.4"
 
 if [ "$current_device" == "$device_1" ]; then
-  SwitchAudioSource -t output -s "$device_2"
-  sketchybar --set $NAME icon="󰓃"
-else
-  SwitchAudioSource -t output -s "$device_1"
+  /opt/homebrew/bin/SwitchAudioSource -t output -s "$device_2"
   sketchybar --set $NAME icon="󰋋"
+else
+  /opt/homebrew/bin/SwitchAudioSource -t output -s "$device_1"
+  sketchybar --set $NAME icon="󰓃"
 fi
