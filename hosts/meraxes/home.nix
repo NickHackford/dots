5085fd32a063da1f17e8ba2eac86056601078d01
Nick Hackford <Nick.Hackford@gmail.com>
2025-03-15 22:51:16 -0400

    feat: improve note syncing

--- hosts/meraxes/home.nix
+++ hosts/meraxes/home.nix
@@ -11,6 +11,29 @@
 
   programs.home-manager.enable = true;
 
+  systemd.user.timers.autoCommitNotes = {
+    Unit = {
+      Description = "Auto commit notes timer";
+    };
+    Timer = {
+      OnCalendar = "*:0/1";
+      Persistent = true;
+    };
+    Install = {
+      WantedBy = ["timers.target"];
+    };
+  };
+  systemd.user.services.autoCommitNotes = {
+    Unit = {
+      Description = "Auto commit notes service";
+    };
+    Service = {
+      ExecStart = "${config.home.homeDirectory}/.local/bin/auto_commit_notes.sh";
+      StandardOutput = "file:/tmp/.auto_commit_notes.log";
+      StandardError = "file:/tmp/.auto_commit_notes.log";
+    };
+  };
+
   home.file = {
     ".gitconfig.local" = {
       text = ''
