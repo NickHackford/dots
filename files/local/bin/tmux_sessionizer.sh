443d3862c481c34840021a5d70e843078fde8e27
Nick Hackford <nick.hackford@gmail.com>
2025-04-04 23:48:27 -0400

    revert(zsh): old way was better

--- files/local/bin/tmux_sessionizer.sh
+++ files/local/bin/tmux_sessionizer.sh
@@ -16,12 +16,12 @@ tmux_running=$(pgrep tmux)
 zoxide add "$selected"
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s $selected_name -c $selected "zsh -i -c nvim"
+  tmux new-session -s $selected_name -c $selected "nvim; exec zsh -i"
   exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-  tmux new-session -ds $selected_name -c $selected "zsh -i -c nvim"
+  tmux new-session -ds $selected_name -c $selected "nvim; exec zsh -i"
 fi
 
 if [[ -z $TMUX ]]; then
