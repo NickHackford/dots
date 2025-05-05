eee7586c5671e37d54866e4959a55e2cc341efa5
Nick Hackford <Nick.Hackford@gmail.com>
2025-05-05 16:14:38 -0400

    tweak: vim opener

--- files/local/bin/vim_opener.sh
+++ files/local/bin/vim_opener.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-    selected=$(find -L ~/ ~/.config ~/src -maxdepth 3 \( -name node_modules -o -name Library \) -prune -o -type f -print | uniq | fzf)
+    selected=$(find -L ~/ ~/.config ~/src -maxdepth 3 \( -name node_modules -o -name Library \) -prune -o -type f -print | sort | uniq | fzf)
 fi
 
 if [[ -z $selected ]]; then
