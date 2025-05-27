0cba88b3874d9df859a873c77da2738f72e21688
Nick Hackford <nick.hackford@gmail.com>
2025-05-26 23:41:53 -0400

    tweak(neogit)

--- files/config/nvim/lua/plugins/neogit.lua
+++ files/config/nvim/lua/plugins/neogit.lua
@@ -1,19 +1,32 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
 
-    "nvim-telescope/telescope.nvim",
-  },
-  config = function()
-    local neogit = require("neogit")
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		local neogit = require("neogit")
 
-    vim.keymap.set("n", "<leader>gG", function()
-      neogit.open({ kind = "vsplit" })
-    end, { desc = "Git", noremap = true, silent = true, nowait = true })
-    vim.keymap.set("n", "<leader>gL", function()
-      neogit.open({ "log", kind = "vsplit" })
-    end, { desc = "Git log", noremap = true, silent = true, nowait = true })
-  end,
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "Neogit*",
+			callback = function()
+				vim.keymap.set("n", "<leader>goc", function()
+					local word = vim.fn.expand("<cword>")
+					OpenInGithub(word)
+				end, { buffer = true, desc = "Git open commit in GitHub" })
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>gG", function()
+			neogit.open({ kind = "vsplit" })
+		end, { desc = "Git", noremap = true, silent = true, nowait = true })
+		vim.keymap.set("n", "<leader>gL", function()
+			neogit.open({ "log", kind = "vsplit" })
+		end, { desc = "Git log", noremap = true, silent = true, nowait = true })
+		vim.keymap.set("n", "<leader>gf", function()
+			neogit.open({ "log", "--", vim.fn.expand("%") }, { kind = "vsplit" })
+		end, { desc = "Git log for current file", noremap = true, silent = true, nowait = true })
+	end,
 }
