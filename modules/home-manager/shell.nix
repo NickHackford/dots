6e409f3ddfc36cf779fbcb8c22cb510cfdcbd0d0
Nick Hackford <nhackford@hubspot.com>
2025-07-07 11:22:56 -0400

    tweak(worktrees): iterate on worktree scripts

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -220,7 +220,7 @@ in {
             ''
           )
           + lib.optionalString config.isHubspot ''
-              alias -- ghe='GH_HOST=git.hubteam.com gh'
+              alias -- br='NODE_ARGS="--max_old_space_size=8192" bend reactor serve --UNSUPPORTED_LOCAL_DEV_SETTING bend-webpack.enableFastRefresh --UNSUPPORTED_LOCAL_DEV_SETTING bend-webpack.useWebpack5 --UNSUPPORTED_LOCAL_DEV_SETTING bend-webpack.devtool=eval-source-map $@ --ts-watch --update --enable-tools --run-tests'
 
             ~/.hubspot/shellrc
           '';
