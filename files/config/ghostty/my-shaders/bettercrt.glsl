798844104da49c62bcfef5811a3782b0f37cb92e
Nick Hackford <Nick.Hackford@gmail.com>
2025-08-05 11:03:11 -0400

    tweak(ghostty)

--- files/config/ghostty/my-shaders/bettercrt.glsl
+++ files/config/ghostty/my-shaders/bettercrt.glsl
@@ -8,8 +8,8 @@
 // - Removed tint
 // - Made the boundaries match ghostty's background color
 
-float warp = 0.10; // simulate curvature of CRT monitor
-float scan = 0.90; // simulate darkness between scanlines
+float warp = 0.00; // simulate curvature of CRT monitor
+float scan = 1.00; // simulate darkness between scanlines
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
