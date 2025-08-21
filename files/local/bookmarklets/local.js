cd6d68a699732ab2a0ebd3abd39038fe0b9ce4e3
Nick Hackford <nhackford@hubspot.com>
2025-08-21 16:29:17 -0400

    tweak(bookmarklet)

--- files/local/bookmarklets/local.js
+++ files/local/bookmarklets/local.js
@@ -3,5 +3,7 @@ if (currentUrl.includes("https://app.")) {
   currentUrl = currentUrl.replace("https://app.", "https://local.");
 } else if (currentUrl.includes("https://local.")) {
   currentUrl = currentUrl.replace("https://local.", "https://app.");
+  // Remove port numbers when switching to prod
+  currentUrl = currentUrl.replace(/:(\d+)/, '');
 }
 window.open(currentUrl, "_blank");
