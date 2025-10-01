8f7193dd8f71eaf5d2fd432146a5220c77e04a45
Nick Hackford <nick.hackford@gmail.com>
2025-09-30 22:48:03 -0400

    tweak(quickshell): switch to caelestia package

--- modules/home-manager/quickshell.nix
+++ modules/home-manager/quickshell.nix
@@ -1,22 +1,18 @@
 {
   pkgs,
   inputs,
+  config,
   ...
 }: {
   home.packages = [
     inputs.quickshell.packages.${pkgs.system}.default
-    pkgs.material-symbols
+    inputs.caelestia-shell.packages.${pkgs.system}.default
   ];
 
   home.file = {
-    "caelestia" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "caelestia-dots";
-        repo = "shell";
-        rev = "3a5f615f509fbff895846ecd3a606eb2c7192cf8";
-        sha256 = "EE9clP2JXbImNr6wjDVk1JumBr8RaMecvi7G07Sbuyg=";
-      };
-      target = ".config/quickshell/caelestia";
+    "nick" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dots/files/config/quickshell/nick";
+      target = ".config/quickshell/nick";
       recursive = true;
     };
   };
