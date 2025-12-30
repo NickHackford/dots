93f8b7d87e052429f72f359991f15b2974e562fe
Nick Hackford <nick.hackford@gmail.com>
2025-12-30 15:05:40 -0500

    tweak(vms): remove outdated setting

--- modules/nixos/vms.nix
+++ modules/nixos/vms.nix
@@ -9,17 +9,6 @@
       qemu = {
         runAsRoot = true;
         package = pkgs.qemu_kvm;
-        # ovmf = {
-        #   enable = true;
-        #   # packages = [
-        #   #   (pkgs.OVMF.override {
-        #   #     secureBoot = true;
-        #   #     tpmSupport = true;
-        #   #   })
-        #   #   .fd
-        #   # ];
-        # };
-        # swtpm.enable = true;
       };
     };
     spiceUSBRedirection.enable = true;
