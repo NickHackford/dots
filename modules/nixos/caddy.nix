720b4490d8bf9c9a319810e8bd5fee82cee194cc
Nick Hackford <nick.hackford@gmail.com>
2026-01-10 18:50:19 +0000

    feat(caddy): add static site

--- modules/nixos/caddy.nix
+++ modules/nixos/caddy.nix
@@ -2,27 +2,42 @@
   services.caddy = {
     enable = true;
 
+    virtualHosts."http://hackford.us" = {
+      serverAliases = [
+        "hackford.us"
+        "tv.hackford.us"
+        "ads.hackford.us"
+      ];
+
+      extraConfig = ''
+        @bare host hackford.us tv.hackford.us ads.hackford.us
+        redir @bare https://www.{host}{uri} permanent
+      '';
+    };
+
     virtualHosts."https://hackford.us" = {
       serverAliases = [
         "www.hackford.us"
         "www.tv.hackford.us"
-        "www.dash.hackford.us"
         "www.ads.hackford.us"
+        "www.dash.hackford.us"
       ];
 
       extraConfig = ''
-        @bare host hackford.us tv.hackford.us dash.hackford.us ads.hackford.us
+        @bare host hackford.us tv.hackford.us ads.hackford.us
         redir @bare https://www.{host}{uri} permanent
 
-        # Main Jellyfin
-        @jellyfin host www.hackford.us www.tv.hackford.us
-        handle @jellyfin {
-          reverse_proxy localhost:8096
+        # Main
+        @main host www.dash.hackford.us
+        handle @main {
+          root * /var/www
+          file_server
         }
 
-        @dash host www.dash.hackford.us
-        handle @dash {
-          reverse_proxy localhost:8000
+        # Jellyfin
+        @jellyfin host www.tv.hackford.us
+        handle @jellyfin {
+          reverse_proxy localhost:8096
         }
 
         @adguard host www.ads.hackford.us
@@ -38,4 +53,13 @@
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
+
+  system.activationScripts.copyDashStatic = {
+    text = ''
+      mkdir -p /var/www
+      cp -r ${../../www}/* /var/www/
+      chown -R caddy:caddy /var/www/
+    '';
+    deps = [];
+  };
 }
