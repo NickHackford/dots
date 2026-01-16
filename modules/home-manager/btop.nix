b2c5dfd4f63b9800403e6b64f67f53459c854c4f
Nick Hackford <nick.hackford@gmail.com>
2026-01-16 12:11:52 -0500

    feat(btop): enable CUDA support and add GPU monitoring

--- modules/home-manager/btop.nix
+++ modules/home-manager/btop.nix
@@ -6,14 +6,17 @@
 }: {
   programs.btop = {
     enable = true;
+    package = pkgs.btop.override {
+      cudaSupport = true;
+    };
     settings = {
       theme_background = false;
       rounded_corners = true;
       vim_keys = true;
       background_update = false;
       temp_scale = "fahrenheit";
-      presets = "cpu:0:default,mem:0:default";
-      # TODO: add preset for gpu
+      presets = "cpu:0:default,mem:0:default,gpu0:0:default";
+      shown_boxes = "cpu mem net proc gpu0";
     };
   };
 }
